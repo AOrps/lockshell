43412e25ed394a2874d167d47f23b10496e04314
AOrps <aorbeandrews@gmail.com>
2024-03-17 03:04:21 -0400

    removed rust build things and modified gitignore
    Signed-off-by: AOrps <aorbeandrews@gmail.com>

--- target/debug/build/signal-hook-a5be4ab7475ec43a/build_script_build-a5be4ab7475ec43a.d
+++
@@ -1,5 +0,0 @@
-/home/aorps/projects/lockshell/target/debug/build/signal-hook-a5be4ab7475ec43a/build_script_build-a5be4ab7475ec43a: /home/aorps/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-0.3.17/build.rs
-
-/home/aorps/projects/lockshell/target/debug/build/signal-hook-a5be4ab7475ec43a/build_script_build-a5be4ab7475ec43a.d: /home/aorps/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-0.3.17/build.rs
-
-/home/aorps/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-0.3.17/build.rs:
--- target/debug/deps/lockshell-adb8f02798faf0e6.d
+++
@@ -1,5 +0,0 @@
-/home/aorps/projects/lockshell/target/debug/deps/lockshell-adb8f02798faf0e6: src/main.rs
-
-/home/aorps/projects/lockshell/target/debug/deps/lockshell-adb8f02798faf0e6.d: src/main.rs
-
-src/main.rs:
